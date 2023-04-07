73ae16c351422c95044ca7ad805d39860fe9b20b
Dmitry V. Luciv <dluciv@spbau.ru>
2023-04-07 10:12:48 +0300

    Separate gen whole and concatenation

--- generator/whole.rb
+++ generator/whole.rb
@@ -5,6 +5,7 @@ require_relative './misc.rb'
 def gen_whole(sections)
   win = is_os_windows?
   suffix, scall = win ? ['bat', 'call'] : ['sh', '.']
+
   File::open(File::join("..", "sections",  "_gen_whole.#{suffix}"), "w:UTF-8") do |f|
     if not win then
       f.puts "#!/bin/bash\n"
@@ -21,11 +22,22 @@ def gen_whole(sections)
       xelatex _toc.tex
       xelatex _toc.tex
 
+      #{scall} __concat_proceedings.#{suffix}
+      TOC
+  end
+
+  File::open(File::join("..", "sections",  "__concat_proceedings.#{suffix}"), "w:UTF-8") do |f|
+    if not win then
+      f.puts "#!/bin/bash\n"
+    end
+
+    f.puts <<~TOC
       pdftk _a_begin.pdf #{sections.map {|s| s.pdfname}.join(' ')} _toc.pdf _z_end.pdf cat output spisok.pdf
       TOC
   end
 
   if not win then
     File::u_plus_x File::join("..", "sections", "_gen_whole.sh")
+    File::u_plus_x File::join("..", "sections", "__concat_proceedings.sh")
   end
 end
